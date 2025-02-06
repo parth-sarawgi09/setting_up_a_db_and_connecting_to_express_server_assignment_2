9dfeed3ffa75b2942832b4ae4652798d4c441694
parth-sarawgi09 <parth.sarawgi@kalvium.community>
2025-02-06 11:32:19 +0530

    updated

--- index.js
+++ index.js
@@ -24,4 +24,4 @@ app.post("/api/users", async (req, res) => {
   }
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
